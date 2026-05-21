5c62c10d677605cb009bb991dfb309916bb0d621
AntsaIsMe <antsajony@gmail.com>
2026-05-21 10:55:44 +0300

    Ui correction

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,63 +1,60 @@
 #!/usr/bin/env node
 
-import inquirer from  'inquirer'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import {questions} from '../src/questions/envQuest.js'
-import { modifFile } from '../src/utils/files.js'
-import toString from '../src/utils/toString.js'
-import fs from 'fs-extra'
-import depInstall from '../src/utils/depInstall.js'
+import inquirer from 'inquirer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { questions } from '../src/questions/envQuest.js';
+import { modifFile } from '../src/utils/files.js';
+import toString from '../src/utils/toString.js';
+import fs from 'fs-extra';
+import depInstall from '../src/utils/depInstall.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const source = path.join(__dirname, '..', 'templates')
-const userPath = process.cwd()
-
+const source = path.join(__dirname, '..', 'templates');
+const userPath = process.cwd();
 const packageJsonPath = path.join(userPath, 'package.json');
 
 async function run() {
   try {
-      const answers = await inquirer.prompt(questions);
-      
-      const { uploadFeature, ...envAnswers } = answers;
-      const envContent = toString(answers)
+    const answers = await inquirer.prompt(questions);
+    
+    const uploadChecked = answers.personalisable?.includes('upload');
 
-      const cible = path.join(__dirname, '..', 'templates', '.env');
+    const { personalisable, ...envAnswers } = answers;
+    const envContent = toString(envAnswers);
 
-      await modifFile(cible, envContent);
+    const cible = path.join(__dirname, '..', 'templates', '.env');
+    await modifFile(cible, envContent);
 
-      // copy templates
-      await fs.copy(source, userPath)
+    // copy templates
+    await fs.copy(source, userPath);
 
+    if (!uploadChecked) {
+      const unusedMiddleware = path.join(userPath, 'src', 'middlewares', 'upload.js');
+      await fs.remove(unusedMiddleware);
+    } else {
+      // add upload dir
+      await fs.ensureDir(path.join(userPath, 'uploads'));
 
-      if (!uploadFeature) {
-        const unusedMiddleware = path.join(userPath, 'src', 'middlewares', 'upload.js');
-        await fs.remove(unusedMiddleware);
-      } else {
-        // add upload dir
-          await fs.ensureDir(path.join(userPath, 'uploads'));
+      const packageJson = await fs.readJson(packageJsonPath);
 
-          const packageJson = await fs.readJson(packageJsonPath);
+      packageJson.dependencies = packageJson.dependencies || {};
+      packageJson.dependencies['multer'] = '^1.4.5-lts.1';
 
-          packageJson.dependencies = packageJson.dependencies || {}
-          packageJson.dependencies['multer'] = '^1.4.5-lts.1';
+      await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 });
+    }
 
-          await fs.writeJson(packageJsonPath, packageJson, {spaces : 2})
-
-      }
-
-      await depInstall(userPath);
+    await depInstall(userPath);
 
   } catch (error) {
-      if (error.isTtyError) {
-          console.log("Problème d'affichage du terminal.");
-      } else {
-          console.log("Une erreur est survenue :", error);      
-      }
+    if (error.isTtyError) {
+      console.log("Problème d'affichage du terminal.");
+    } else {
+      console.log("Une erreur est survenue :", error);      
+    }
   }
 }
 
-run()
+run();
--- src/questions/envQuest.js
+++ src/questions/envQuest.js
@@ -1,55 +1,102 @@
-import { validNormChar, validNum } from '../utils/validator.js'
+import chalk from 'chalk'; 
+import { validNormChar, validNum } from '../utils/validator.js';
+import inquirer from 'inquirer';
 
 export const questions = [
-    {
-      type: 'input',
-      name: 'PORT',
-      message: 'Sur quel port doit tourner votre serveur Express ?',
-      default: '3000',
-      validate: validNum
-    },
+  {
+    type: 'html',
+    name: 'nodibranch_logo',
+    message: '',
+    when: () => {
+      // Dégradé de couleurs du haut vers le bas harmonisé
+      const c1 = chalk.hex('#00f5d4'); // Cyan très flash
+      const c2 = chalk.hex('#72efdd'); // Turquoise clair
+      const c3 = chalk.hex('#56cfe1'); // Bleu lagon
 
-    {
-      type: 'input',
-      name: 'DB_HOST',
-      message: 'Quel est l\'hôte de votre base de données MySQL ?',
-      default: 'localhost'
-    },
-    {
-      type: 'input',
-      name: 'DB_NAME',
-      message: 'Quel est le nom de la base de données ?',
-      default: 'mon_backend_db',
-      validate : validNormChar
-    },
+      const textGreen = chalk.bgHex('#52b788').white.bold;
+      const textCyan = chalk.bgHex('#00b4d8').white.bold;
 
-    {
-        type: 'input',
-        name: 'DB_PORT',
-        message: 'Quel est le port de la database ?',
-        default: '3306',
-        validate: 'validator'
-    },
-
-    {
-      type: 'input',
-      name: 'DB_USER',
-      message: 'Quel est l\'utilisateur MySQL ?',
-      default: 'root'
-    },
-
-    {
-        type: "password",
-        name: 'DB_PASSWORD',
-        message: 'Quel est le mot de passe MySQL ?',
-        mask: '*',
-        default: '',
-    },
-    {
-        type: 'confirm',
-        name: 'uploadFeature',
-        message: 'Souhaites-tu intégrer une fonctionnalité d\'upload de fichiers (via Multer) ?',
-        default: false
-      },
-  ];
-
+      console.log(
+        c1("    ...................................... \n") +
+        c1("  ......___________........../.../.....   \n") +
+        c2(" ....../           \\........|___/......   \n") +
+        c2(" .....||            \\....../ _ _ \\......   \n") +
+        c3(" ......\\             \\____/   o  /.......  \n") +
+        c3("  ......\\           /           /......... \n") +
+        c3("   ......\\_________/           /......... \n") +
+        c3("   .......\\___________________/.........  \n") +
+        c2("   .......................................  \n") +
+        c2("  .......  ") + textGreen(" N O D I ") + textCyan(" B R A N C H ") + c1("  ............\n") +
+        c1(" .............................................\n")
+      );
+      
+      return false;
+    }
+  },
+  {
+    type: 'input',
+    name: 'PORT',
+    message: 'Port du serveur Express :', 
+    default: '3000',
+    validate: validNum
+  },
+  {
+    type: 'input',
+    name: 'DB_NAME',
+    message: 'Nom de la base de données :', 
+    default: 'tempName',
+    validate: validNormChar
+  },
+  {
+    type: 'html', 
+    name: 'spacer_top',
+    message: '',
+    when: () => {
+      console.log(''); 
+      return false;  
+    }
+  },
+  {
+    type: 'checkbox',
+    name: 'personalisable',
+    prefix: `${chalk.green('\n?')}`,
+    suffix: '\n',
+    message: chalk.bgCyan(" Choisissez ce que vous allez personnaliser : "), // 🛠️ Nettoyage du \n interne qui décalait le prompt
+    choices: [
+      new inquirer.Separator(' '),
+      { name: 'Personnaliser le DB host, le port, user, password', value: 'dbcustom'},
+      { name: 'Integrer l\'upload fichier (Multer)', value: 'upload' },
+      new inquirer.Separator(' '),
+    ]
+  }, 
+  {
+    type: 'input',
+    name: 'DB_HOST',
+    message: '    Quel est l\'hôte de la base de données ?',
+    default: 'localhost',
+    when: (answers) => answers.personalisable?.includes('dbcustom')
+  },
+  {
+    type: 'input',
+    name: 'DB_PORT',
+    message: '    Quel est le port de la base de données ?',
+    default: '3306',
+    validate: validNum,
+    when: (answers) => answers.personalisable?.includes('dbcustom')
+  },
+  {
+    type: 'input',
+    name: 'DB_USER',
+    message: '    Quel est l\'utilisateur MySQL ?',
+    default: 'root',
+    when: (answers) => answers.personalisable?.includes('dbcustom')
+  },
+  {
+    type: 'password',
+    name: 'DB_PASSWORD',
+    message: '    Quel est le mot de passe MySQL ?',
+    mask: '*',
+    default: '',
+    when: (answers) => answers.personalisable?.includes('dbcustom')
+  },
+];
